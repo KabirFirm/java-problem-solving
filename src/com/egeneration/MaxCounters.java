4cf091be3e07bf3dc8fa5b2c185d87f14610e5a3
Humayun Kabir <humayun.kabir@egeneration.co>
2022-06-27 14:30:20 +0600

    MaxCounters-Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum

--- src/com/egeneration/MaxCounters.java
+++ src/com/egeneration/MaxCounters.java
@@ -57,5 +57,86 @@ N and M are integers within the range [1..100,000];
 each element of array A is an integer within the range [1..N + 1].
  */
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class MaxCounters {
+
+    public static int[] solution(int N, int[] A){
+
+        /*// correctness = 100%, performance = 20%, score = 55%
+        int[] result = new int[N]; // create and initiate result array of length N and initial value 0
+        HashMap <Integer, Integer> occurrencesMap = new HashMap<Integer, Integer>(); //hash map to set unique number and store max value
+
+        // find unique number from given array A, and set each unique number's max value to 0(zero)
+        for (int i = 0; i < A.length; i++){
+            if(A[i] <= N) occurrencesMap.put(A[i], 0); //set max counter for i element
+        }
+
+        int globalMax = 0; // set globalMax value to 0
+
+        for (int i = 0; i < A.length; i++) {
+            int tempValue = A[i];
+            // if number of i index is less than or equal to N
+            if(A[i] <= N){
+                // get this individual number's max value
+                int maxNumber = occurrencesMap.get(A[i]);
+                maxNumber++; // increment individual number's value by 1
+
+                //update globalMax
+                if(globalMax < maxNumber) globalMax = maxNumber;
+
+                // store max number for each individual number
+                occurrencesMap.put(A[i],maxNumber);
+
+                result[tempValue - 1] = maxNumber;
+            }else if(A[i] == N + 1){ // if number of i index is equal to N + 1
+
+                // set entire result array to max value
+                for (int j = 0; j < N; j++){
+                    result[j] = globalMax;
+                }
+                //set each unique number max value to global Max
+                for (int num: occurrencesMap.keySet()) {
+                    occurrencesMap.put(num,globalMax);
+                }
+            }
+        }
+        return result;*/
+        int [] counters = new int[N];
+
+        int maxCounter = 0; //for the next re-set will know what high score to set all values
+        int lastResetCounter = 0; //for setting values that were never explicitly set - at the end
+
+        for(int i = 0; i < A.length; i++) {
+            if(A[i] <= N) {
+                if(counters[A[i]-1] < lastResetCounter) {
+                    counters[A[i]-1] = lastResetCounter;
+                }
+                //bring it up to last reset value
+                counters[A[i]-1]++;
+
+                // store max counter in case need to set all counters to this value on next reset
+                if(counters[A[i]-1] > maxCounter) {
+                    maxCounter = counters[A[i]-1];
+                }
+            } else {
+                //keep track of last reset counter
+                lastResetCounter = maxCounter;
+            }
+        }
+
+        //set all values to last reset value that was never explicitly changed after last reset
+        for(int i=0; i<counters.length; i++) {
+            if(counters[i] < lastResetCounter) {
+                counters[i]  = lastResetCounter;
+            }
+        }
+
+        return counters;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(5, new int[] {3,4,4,6,1,4,4})));
+    }
 }
