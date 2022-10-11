df0ab8420be61427bdeef52beffe6a27aae69b9a
Humayun Kabir <humayun.kabir@egeneration.co>
2022-10-11 10:24:23 +0600

    Recursion - Iterative factorial

--- src/com/egeneration/Recursion.java
+++ src/com/egeneration/Recursion.java
@@ -2,7 +2,7 @@ package com.egeneration;
 
 public class Recursion {
     // iterative factorial
-    public static int factorial(int n) {
+    public static int IterativeFactorial(int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result = result * i;
@@ -10,8 +10,19 @@ public class Recursion {
         return result;
     }
 
+    // recursive factorial
+    public static int RecursiveFactorial(int n) {
+        // base case
+        if(n == 0){
+            return 1;
+        }
+        // recursive case
+        return n * RecursiveFactorial(n - 1);
+    }
+
     public static void main(String[] args) {
-        int number = 5;
-        System.out.println(factorial(number));
+        int number = 4;
+        //System.out.println(IterativeFactorial(number));
+        System.out.println(RecursiveFactorial(number));
     }
 }
