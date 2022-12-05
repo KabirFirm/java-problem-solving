31b126c87b225f801267832ae8539ef3af1813cd
Humayun Kabir <humayun.kabir@egeneration.co>
2022-12-05 14:26:49 +0600

    oddOccurrenceInArray revised

--- src/com/egeneration/Playground.java
+++ src/com/egeneration/Playground.java
@@ -1,8 +1,12 @@
 package com.egeneration;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Playground {
+
+    // cyclicRotation
     public static int[] cyclicRotation(int[] A, int K) {
         if(A.length < 2 || K == 0)  return A;
         for(int i = 0; i < K; i++) {
@@ -15,9 +19,39 @@ public class Playground {
         return A;
     }
 
+    // OddOccurrenceInArray
+
+    public static int oddOccurrenceInArray(int[] A) {
+        HashMap<Integer, Integer> occuranceMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < A.length; i++){
+            if(occuranceMap.containsKey(A[i])) {
+                int occurance = occuranceMap.get(A[i]);
+                occurance++;
+                occuranceMap.put(A[i],occurance);
+            }else {
+                occuranceMap.put(A[i],1);
+            }
+        }
+        for(Object keySet : occuranceMap.keySet()){
+            int occurances = occuranceMap.get(keySet);
+            if(occurances % 2 != 0) return (int) keySet;
+        }
+        return 0;
+        //throw new RuntimeException("you must get this");
+    }
+
+
     public static void main(String[] args) {
+
+        /*
+        // cyclicRotation function section
         int[] inputArray = {3,8,9,0};
         int numberOfShift = 2;
         System.out.println(Arrays.toString(cyclicRotation(inputArray,numberOfShift)));
+        */
+
+        // oddOccurrenceInArray function section
+        int[] inputArray = {1000000, 1000000, 1000000, 2000000, 1000000, 30000000, 5000000, 2000000, 30000000};
+        System.out.println(oddOccurrenceInArray(inputArray));
     }
 }
